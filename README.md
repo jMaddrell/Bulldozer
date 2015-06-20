a1e135204023c4be5093f5a8f7cbebe274b466e0
Jordan <jmaddrell@me.com>
2015-06-20 17:10:26 +0100

    Added README and short-hand cli switches

---
+++ README.md
@@ -0,0 +1,12 @@
+# Bulldozer
+Angular.JS (or React.JS) forum powered by JAX-RS
+
+## Project Setup
+
+- Install PostgreSQL
+- Create the a user for bulldozer + database (make sure to set a password!)
+- Edit BulldozerArgs to use the correct db or use the switches
+-- java -jar Bulldozer.jar -p [properties files] -dbhost [host] -dbport [port] -dbuser [user] -dbpass [pass] -dbname [name]
+
+- Visit [Bulldozer](http://127.0.0.1:8080/ "Bulldozer")
+
--- src/main/java/de/refactorco/Bulldozer/util/BulldozerArgs.java
+++ src/main/java/de/refactorco/Bulldozer/util/BulldozerArgs.java
@@ -7,21 +7,21 @@ import com.lexicalscope.jewel.cli.Option;
  * Edited: 20/06/15.
  */
 public interface BulldozerArgs  {
-    @Option(description = "system configuration bootstrap file", defaultValue = "bulldozer.properties")
+    @Option(description = "system configuration bootstrap file", defaultValue = "bulldozer.properties", shortName = "p")
     String getConfigurationBootstrap();
 
-    @Option(description = "postgresql server host", defaultValue = "localhost")
+    @Option(description = "postgresql server host", defaultValue = "localhost", shortName = "dbhost")
     String getPostgresHost();
 
-    @Option(description = "postgresql server port", defaultValue = "5432")
+    @Option(description = "postgresql server port", defaultValue = "5432", shortName = "dbport")
     int getPostgresPort();
 
-    @Option(description = "postgresql server user", defaultValue = "bulldozer")
+    @Option(description = "postgresql server user", defaultValue = "bulldozer", shortName = "dbuser")
     String getPostgresUser();
 
-    @Option(description = "postgresql server password", defaultValue = "bulldozer123")
+    @Option(description = "postgresql server password", defaultValue = "bulldozer123", shortName = "dbpass")
     String getPostgresPassword();
 
-    @Option(description = "postgresql server database", defaultValue = "bulldozer")
+    @Option(description = "postgresql server database", defaultValue = "bulldozer", shortName = "dbname")
     String getPostgresDatabase();
 }
